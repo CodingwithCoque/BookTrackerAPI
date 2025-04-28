8ead62a21d32f371e36f6c179651e3dd6f89fdcc
Christopher Lagunas <148503662+CodingwithCoque@users.noreply.github.com>
2025-04-27 20:17:08 -0700

    Delete src/books directory

--- src/books/books.queries.ts
+++
@@ -1,8 +0,0 @@
-export const queries = {
-    getAll: "SELECT * FROM books",
-    getById: "SELECT * FROM books WHERE id = ?",
-    create: "INSERT INTO books (title, author, genre, year_published) VALUES (?, ?, ?, ?)",
-    update: "UPDATE books SET title = ?, author = ?, genre = ?, year_published = ? WHERE id = ?",
-    delete: "DELETE FROM books WHERE id = ?",
-  };
-  
